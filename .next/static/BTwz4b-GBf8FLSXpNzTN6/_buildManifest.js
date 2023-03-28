684a65719424089c7cd2e835dc9a2dc3a74d9669
Shivam Chaudhary <shivamc021999@gmail.com>
2023-03-28 13:26:54 +0530

    Build files v2

---
+++ .next/static/BTwz4b-GBf8FLSXpNzTN6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9cbc223294a089d9.css","static/chunks/pages/index-0448b54d7cedd975.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
