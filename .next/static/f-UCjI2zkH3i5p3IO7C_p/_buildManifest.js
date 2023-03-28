f5fa0a69f086bd70055885d30d057fe77157fcb2
Shivam Chaudhary <shivamc021999@gmail.com>
2023-03-28 18:40:50 +0530

    build automation

--- .next/static/f-UCjI2zkH3i5p3IO7C_p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0d4b51e6ff6808b3.css","static/chunks/pages/index-fa71c9a056bfb693.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/Common/Header.js
+++ src/Common/Header.js
@@ -19,19 +19,19 @@ const Header = () => {
 				<Link href="/">
 					<a className={styles.headerLink}>
 						<img
-							src={"/igniteassets/icons/g20-logo.png"}
+							src={"/ignite/assets/icons/g20-logo.png"}
 							height="20%"
 							width="20%"
 							className={styles.iitgn_logo_header}
 						/>
 						<img
-							src={"/igniteassets/icons/iitgn-logo-white.png"}
+							src={"/ignite/assets/icons/iitgn-logo-white.png"}
 							height="20%"
 							width="20%"
 							className={styles.iitgn_logo_header}
 						/>
 						<img
-							src={"/igniteassets/icons/logo_lal_minar.png"}
+							src={"/ignite/assets/icons/logo_lal_minar.png"}
 							height="20%"
 							width="20%"
 							className={styles.header_logo}
